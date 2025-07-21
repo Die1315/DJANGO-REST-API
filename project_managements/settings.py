9f6b712f29047d82d7cf1ec04ea44179842a488b
Diego Guauta <diego.g0912@hotmail.es>
2025-07-21 12:51:04 -0500

    Adds environment variables for security
    Enhances security by utilizing environment variables
    to store sensitive information such as the secret key
    and database credentials.
    
    This prevents hardcoding sensitive data directly into
    the settings file, reducing the risk of exposure.

--- project_managements/settings.py
+++ project_managements/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s@g4ywv^rq=_c(3f1py#wz^_isov*(z35c!8h8id4$kv2%awkz' # noqa: E501
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -91,11 +92,11 @@ WSGI_APPLICATION = 'project_managements.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'project_management',
-        'USER': 'postgres',
-        'PASSWORD': '0g1dd13',
-        'HOST': 'localhost',
-        'PORT': '5433',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
